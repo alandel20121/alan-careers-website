260f96fed367537b2558100c90c2fe7093b2a3d1
alandel20121 <164709356+alandel20121@users.noreply.github.com>
2024-03-28 23:47:47 +0000

    added dynamic data and API route

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return render_template('home.html')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+  
 if __name__=="__main__":
   app.run(host='0.0.0.0', debug=True)
   
